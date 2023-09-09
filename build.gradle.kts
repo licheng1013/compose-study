e2ad83980ad60d0619fb1bedfa5de3e2be00a077
licheng <2501093733@qq.com>
2023-09-09 18:58:16 +0800

    增加web功能!

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,9 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     // 二维码生成
     maven  ("https://jitpack.io" )
+    // web 框架
+    maven  ("https://maven.pkg.jetbrains.space/public/p/ktor/eap" )
+
 }
 
 kotlin {
@@ -28,6 +31,11 @@ kotlin {
                 implementation(compose.desktop.currentOs)
                 // QRCode 二维码生成
                 implementation("com.github.kenglxn.QRGen:javase:3.0.1")
+                // web 框架
+                implementation("io.ktor:ktor-server-core-jvm:2.3.4")
+                implementation("io.ktor:ktor-server-netty-jvm:2.3.4")
+                implementation("io.ktor:ktor-server-status-pages-jvm:2.3.4")
+                implementation("io.ktor:ktor-server-default-headers-jvm:2.3.4")
             }
         }
         val jvmTest by getting
--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,12 +1,35 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import file.home
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import log.myPrintln
 
 
 fun main() = application {
+    // web 框架
+    // 子线程启动
+    Thread{
+        myPrintln("web 服务启动")
+        embeddedServer(Netty, port = 8000) {
+            routing {
+                get ("/") {
+                    call.respondText("Hello, world!")
+                }
+            }
+        }.start(wait = true)
+    }.start()
+
+
     Window(
         title = "File Sync",
         onCloseRequest = ::exitApplication) {
         home()
     }
+
+
+
 }
