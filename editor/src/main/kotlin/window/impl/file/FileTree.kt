a2472a0d0dae4ec7a07011f8a51f540482938319
licheng <2501093733@qq.com>
2023-09-21 13:48:30 +0800

    增加文件树选择功能!

--- editor/src/main/kotlin/window/impl/file/FileTree.kt
+++ editor/src/main/kotlin/window/impl/file/FileTree.kt
@@ -1,10 +1,16 @@
 package window.impl.file
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import open.FileUtil
 import theme.Theme
@@ -15,11 +21,11 @@ fun FileTree(file: String, offsetX: Int = 0) {
     if (!FileUtil.isDirectory(file)) {
         throw RuntimeException("file is not directory")
     }
-    val name = FileUtil.getFileOrDirName(file)
+    val selected = mutableStateOf(file)
     Column {
         Row {
             Spacer(Modifier.width(offsetX.dp))
-            Text(name, color = Theme.fontColor)
+            text(file, selected)
         }
         Row(Modifier.fillMaxHeight()) {
             Spacer(Modifier.width((offsetX + 8).dp))
@@ -27,14 +33,32 @@ fun FileTree(file: String, offsetX: Int = 0) {
                 val list = FileUtil.listFile(file)
                 list.forEach {
                     if (it.isDirectory) {
-                        Text(it.name, color = Theme.fontColor)
+                        text(it.path, selected)
                     } else {
-                        Text(it.name, color = Theme.fontColor)
+                        text(it.path, selected)
                     }
                 }
             }
         }
-
     }
+}
 
+
+@Composable
+fun text(path: String, selected: MutableState<String>) {
+    var name = FileUtil.getFileOrDirName(path)
+    var selectedColor = if (selected.value == path) Theme.selectedColor else Theme.lightGery
+    Text(
+        name,
+        color = Theme.fontColor,
+        modifier = Modifier.pointerHoverIcon(PointerIcon.Hand)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = {
+                        selected.value = path
+                    }
+                )
+            }
+            .background(selectedColor).fillMaxWidth()
+    )
 }
