acfd1cc06bda1d3dba37142230b87d6d4647af9f
licheng <2501093733@qq.com>
2023-09-27 18:55:57 +0800

    增加浮动窗口示例!

--- study/src/main/kotlin/demo/FloatingWindow.kt
+++ study/src/main/kotlin/demo/FloatingWindow.kt
@@ -1,14 +1,12 @@
 package demo
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
@@ -25,8 +23,8 @@ fun main() = application {
             width = 200.dp,
         ),
         onCloseRequest = ::exitApplication,
-       // resizable = false,
-       // undecorated = true,
+        // resizable = false,
+        // undecorated = true,
         alwaysOnTop = true,
     ) {
         MyFloatingWindow()
@@ -38,6 +36,25 @@ fun MyFloatingWindow() {
     Box(
         modifier = Modifier.height(100.dp).width(200.dp)
     ) {
-        Text("Hello, World!")
+        Column {
+            Text("Hello, World!")
+            //MyTransparentBox()
+        }
+
     }
 }
+
+//@Composable
+//fun MyTransparentBox() {
+//    Box(
+//        modifier = Modifier
+//            .width(200.dp)
+//            .height(100.dp)
+//            .background(Color.Green)
+//            .pointerInput(Unit) {
+//                true // 返回 true 表示事件已经被处理
+//            }
+//    ) {
+//        Text("Hello, pointerInput!")
+//    }
+//}
