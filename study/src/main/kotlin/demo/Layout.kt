b97abdca5f991f286714abff59e72cc36dab37ab
licheng <2501093733@qq.com>
2023-11-21 15:23:52 +0800

    优化示例

--- study/src/main/kotlin/demo/Layout.kt
+++ study/src/main/kotlin/demo/Layout.kt
@@ -3,24 +3,35 @@ package demo
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+
+object LayoutState{
+    val inputText = mutableStateOf("")
+}
+
+
 // 这个示例展示了基本布局
-// Modifier.weight(0.5f) 两个子组件都一样则两边分
 @Composable
 @Preview
 fun LayoutDemo() {
-    MaterialTheme {
-        Row {
-            Text("水平布局", modifier = Modifier.weight(0.5f))
-            Column(modifier = Modifier.weight(0.5f)) {
+    MaterialTheme { // Material 主题
+        Row { // 水平布局组件
+            // 文字组件，Modifier.weight(1f) 设置组件大小权重
+            Text(LayoutState.inputText.value, modifier = Modifier.weight(1f))
+            // 垂直布局组件
+            Column(modifier = Modifier.weight(1f)) {
                 Text("垂直布局")
                 Text("Hello World")
+                OutlinedTextField(LayoutState.inputText.value, onValueChange = {
+                    LayoutState.inputText.value = it
+                })
             }
         }
     }
@@ -32,8 +43,3 @@ fun main() = application {
     }
 }
 
-
-@Composable
-fun MyScrollableRow() {
-
-}
